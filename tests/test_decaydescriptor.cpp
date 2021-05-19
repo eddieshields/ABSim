0c314fbcf037f653bbc2122fb4caf057785d8c3b
Edward Shields <edward.brendan.shields@cern.ch>
2021-05-19 22:39:37 +0100

    Fixed broken test

--- tests/test_decaydescriptor.cpp
+++ tests/test_decaydescriptor.cpp
@@ -6,7 +6,7 @@
 using namespace ABSIM;
 
 TEST_CASE( "Test DecayDescriptor", "[DecayDescriptor]") {
-  std::string descriptor_str = "D*+ => { D0 => { KS0 => pi+ pi- } K+ K- } pi+";
+  std::string descriptor_str = "D*+ => { D0 => { K_S0 => pi+ pi- } K+ K- } pi+";
   DecayDescriptor descriptor(descriptor_str);
   std::cout << descriptor << std::endl;
 }
