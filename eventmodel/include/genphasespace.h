356c6e5445709aeefed50195bcab6c9737dafee3
Edward Shields <edward.brendan.shields@cern.ch>
2021-06-30 17:54:52 +0200

    Added more documentation

--- eventmodel/include/genphasespace.h
+++ eventmodel/include/genphasespace.h
@@ -11,21 +11,42 @@
 
 namespace ABSIM {
 
+  /**
+   * @class GenPhaseSpace
+   * 
+   * @brief Implementation of ROOT's TGenPhaseSpace.
+   * 
+   * @details Implementation of ROOT's TGenPhaseSpace algorithm (https://root.cern.ch/doc/master/classTGenPhaseSpace.html)
+   * base on the GENBOD function (W515 from CERNLIB) using the Raubold and Lynch method F. James, Monte Carlo Phase Space, 
+   * CERN 68-15 (1968).
+   * Generated a decay from a mother to N daughters according to a phase-space distrribution.
+   * 
+   * @author Rene Brun
+   * @author Valerio Filippini
+   * @author Edward Shields
+   * @date 30/06/2021
+   */
   class GenPhaseSpace
   {
   friend class Decay;
   private: 
-    int_t n_;
-    real_t mass_[18];
-    real_t beta_[3];
-    real_t teCmTm_;
-    real_t pM_;
-    real_t wtMax_;
-    FourVector* decProd_[18];
+    int_t n_;                   ///< Number of particles to be decayed.
+    real_t mass_[18];           ///< Array of masses of daughter particles.
+    real_t beta_[3];            ///< Betas of decaying particles.
+    real_t teCmTm_;             ///< Center of Mass energyminus total mass.
+    real_t pM_;                 ///< Total mass.
+    real_t wtMax_;              ///< Maximum weigth.
+    FourVector* decProd_[18];   ///< Linematics of the generated particles.
 
+    /**
+     * PDK function.
+     */
     inline real_t pdk_(real_t a, real_t b, real_t c) const;
 
   public:
+  /**
+   * Constructor.
+   */
     GenPhaseSpace() :
       n_( 0 ),
       mass_(),
@@ -33,16 +54,54 @@ namespace ABSIM {
       teCmTm_( 0. ),
       wtMax_( 0. )
     {}
+
+    /**
+     * Copy constructor.
+     */
     GenPhaseSpace(const GenPhaseSpace& gen);
+
     ~GenPhaseSpace() {}
+
+    /**
+     * Move operator.
+     */
     GenPhaseSpace& operator=(const GenPhaseSpace& gen);
 
+    /**
+     * Set the decay.
+     * 
+     * @param p Four momentum vector of mother
+     * @param n Number of decay products
+     * @param mass Array of decay product masses
+     */
     bool SetDecay(FourVector& p, int_t n, const real_t* mass);
+
+    /**
+     * Update the decay to redecay.
+     * 
+     * @param p Four momentum vector of mother
+     */
     bool UpdateDecay(FourVector& p);
+
+    /**
+     * Generate a randim final state.
+     * Return the weight of the current decay.
+     */
     real_t Generate();
+
+    /**
+     * Get the Four momentum vector of a decay product by it's index.
+     */
     FourVector* GetDecay(int_t n);
 
+    /**
+     * Get number of decay products.
+     */
     inline int_t GetN() const { return n_; }
+
+    /**
+     * Get maximum weight.
+     */
     inline real_t GetWtMax() const { return wtMax_; }
   };
 
--- eventmodel/include/particleproperty.h
+++ eventmodel/include/particleproperty.h
@@ -8,28 +8,62 @@
 
 namespace ABSIM {
 
+  /**
+   * @class ParticleProperty
+   * 
+   * @brief Structure to hold a property of a particle.
+   * 
+   * @details For each property of a particle generated there are two relevent pieces of information.
+   * The first is what is the generated value of the property and the second in what is the reconstructed
+   * value of the property. This structure holds both and allows access to both the generated and reconstructed
+   * value through a PropertyType: Property::Generated, and Property::Reconstrcuted.
+   * 
+   * @author Edward Shields
+   * @date 30/06/2021
+   */
   template <typename T>
   struct ParticleProperty {    
-    T gen_;
-    T reco_;
+    T gen_;   ///< Generated value of the property.
+    T reco_;  ///< Reconstructed value of the property.
 
+    /**
+     * Constructor
+     */
     ParticleProperty() :
       gen_( T() ),
       reco_( T() )
     {}
+
+    /**
+     * Constructor
+     * 
+     * @param t Generated value of the property
+     */
     ParticleProperty(const T& t) :
       gen_( t ),
       reco_( t )
     {}
+
+    /**
+     * Constructor
+     * 
+     * @param t_true Generated value of the property
+     * @param t_reco Reconstructed value of the property
+     */
     ParticleProperty(const T& t_true, const T& t_reco) :
       gen_( t_true ),
       reco_( t_reco )
     {}
     ~ParticleProperty() {}
 
-    // Copy constructor.
+    /**
+     * Copy constructor.
+     */
     ParticleProperty(const ParticleProperty<T>& rhs);
-    // Move constructor.
+    
+    /**
+     * Move constructor.
+     */
     ParticleProperty<T>& operator=(const ParticleProperty<T>& rhs);
     ParticleProperty<T>& operator=(const std::tuple<T,T>& rhs);
     ParticleProperty<T>& operator=(const T& rhs)
@@ -40,11 +74,16 @@ namespace ABSIM {
     }
 
     // Operators.
-    ParticleProperty<T> operator+(const ParticleProperty<T>& rhs);
-    ParticleProperty<T> operator-(const ParticleProperty<T>& rhs);
-    ParticleProperty<T> operator*(const ParticleProperty<T>& rhs);
-    ParticleProperty<T> operator/(const ParticleProperty<T>& rhs);
+    ParticleProperty<T> operator+(const ParticleProperty<T>& rhs); //!< Addition operator.
+    ParticleProperty<T> operator-(const ParticleProperty<T>& rhs); //!< Subtraction operator.
+    ParticleProperty<T> operator*(const ParticleProperty<T>& rhs); //!< Multiplication operator.
+    ParticleProperty<T> operator/(const ParticleProperty<T>& rhs); //!< Division operator.
 
+    /**
+     * Access generated or reconstructed value of the property.
+     * 
+     * @param type Type ot be returned: Generated or Reconstructed.
+     */
     T& operator()(Property::Type type)
     {
       switch (type) {
@@ -56,8 +95,8 @@ namespace ABSIM {
       return gen_;
     }
 
-    constexpr T generated()     const { return gen_; }
-    constexpr T reconstructed() const { return reco_; }
+    constexpr T generated()     const { return gen_; }  //!< Return generated value.
+    constexpr T reconstructed() const { return reco_; } //!< Return reconstructed value.
   };
 
 }  // namespace ABSIM
